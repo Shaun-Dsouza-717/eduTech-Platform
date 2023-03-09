d1bfb76176e2411455f728be7a89ebed7f3466d0
Shaun Dsouza <dsouzashaun717@gmail.com>
2023-03-10 00:41:44 +0530

    initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'eduTech',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
